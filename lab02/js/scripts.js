b75a410c30e0b8f942732e6514ed49b5bf8947c9
AbdulhakimZatar <hakimzatar@gmail.com>
2020-07-06 15:26:34 +0300

    add files

---
+++ lab02/js/scripts.js
@@ -0,0 +1,53 @@
+"use strict";
+var userName = prompt('Hi, what is your name?');
+alert('Hi '+userName+', are you ready for quick quiz about me? answer using yes or no');
+
+var a1 = prompt('Do i love coding?');
+if(a1.toLocaleLowerCase() == ("yes" || "y")){
+    alert('Correct');
+    console.log('First answer is correct');
+} else if(a1.toLocaleLowerCase() == ("no" || "n")) {
+    alert('Wrong');
+    console.log('First answer is wrong');
+} else {
+    alert('Please use yes/no or y/n');
+    console.log('Wrong value');
+}
+
+var a2 = prompt('Am i a gamer?');
+if(a2.toLocaleLowerCase() == ("yes" || "y")){
+    alert('Correct');
+    console.log('Second  answer is correct');
+} else if(a2.toLocaleLowerCase() == ("no" || "n")) {
+    alert('Wrong');
+    console.log('Second answer is wrong');
+} else {
+    alert('Please use yes/no or y/n');
+    console.log('Wrong value');
+}
+
+var a3 = prompt('Is travel my favorite hobby?');
+if(a3.toLocaleLowerCase() == ("yes" || "y")){
+    alert('Correct');
+    console.log('Third answer is correct');
+} else if(a3.toLocaleLowerCase() == ("no" || "n")) {
+    alert('Wrong');
+    console.log('Third answer is wrong');
+} else {
+    alert('Please use yes/no or y/n');
+    console.log('Wrong value');
+}
+
+var a4 = prompt("I don't like food, is this right?");
+if(a4.toLocaleLowerCase() == ("yes" || "y")){
+    alert('Wrong');
+    console.log('fourth answer is wrong');
+} else if(a4.toLocaleLowerCase() == ("no" || "n")) {
+    alert('Correct');
+    console.log('fourth answer is correct');
+} else {
+    alert('Please use yes/no or y/n');
+    console.log('Wrong value');
+}
+
+alert('Thanks '+userName+' for your answers, come back later for more quizes');
