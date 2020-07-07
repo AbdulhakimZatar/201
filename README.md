36b30ee12befde32e67c394006049ff805f3c858
AbdulhakimZatar <hakimzatar@gmail.com>
2020-07-07 14:16:40 +0300

    add version 0.02

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# 201
+# 201 Class
+
+
+* [Lab02 | v0.01](lab02)
+- Created homepage with html, css and javascript.
+- added five questions with ability to check if the answer is yes/no and accept answers even if letters are small or capital
+- added welcome message to user.
+
+* [Lab03 | v0.02](lab03)
+- Added top ten games to homepage.
+- Changed the work experience and education into unorder list.
+- Added two more questions.
+- Added counter for correct answers.
+- Added counters for attempts for 6th and 7th questions.
--- lab03/js/scripts.js
+++ lab03/js/scripts.js
@@ -3,11 +3,16 @@
 var userName = prompt('Hi, what is your name?');
 alert('Hi '+userName+', are you ready for quick quiz about me? answer using yes or no');
 
+//counter for correct answers + Array for top four games i love.
+var correctAnswers = 0;
+var topGames = ['gta','arma','squad','fifa'];
+
 //asking user and make sure he/she answer with yes/no
 var a1 = prompt('Do i love coding?');
 if(a1.toLocaleLowerCase() == ("yes" || "y")){
     alert('Correct');
     console.log('First answer is correct');
+    correctAnswers++;
 } else if(a1.toLocaleLowerCase() == ("no" || "n")) {
     alert('Wrong');
     console.log('First answer is wrong');
@@ -20,6 +25,7 @@ var a2 = prompt('Am i a gamer?');
 if(a2.toLocaleLowerCase() == ("yes" || "y")){
     alert('Correct');
     console.log('Second  answer is correct');
+    correctAnswers++;
 } else if(a2.toLocaleLowerCase() == ("no" || "n")) {
     alert('Wrong');
     console.log('Second answer is wrong');
@@ -32,6 +38,7 @@ var a3 = prompt('Is travel my favorite hobby?');
 if(a3.toLocaleLowerCase() == ("yes" || "y")){
     alert('Correct');
     console.log('Third answer is correct');
+    correctAnswers++;
 } else if(a3.toLocaleLowerCase() == ("no" || "n")) {
     alert('Wrong');
     console.log('Third answer is wrong');
@@ -46,7 +53,8 @@ if(a4.toLocaleLowerCase() == ("yes" || "y")){
     console.log('fourth answer is wrong');
 } else if(a4.toLocaleLowerCase() == ("no" || "n")) {
     alert('Correct');
-    console.log('fourth answer is correct');
+    console.log('Fourth answer is correct');
+    correctAnswers++;
 } else {
     alert('Please use yes/no or y/n');
     console.log('Wrong value');
@@ -59,9 +67,55 @@ if(a5.toLocaleLowerCase() == ("yes" || "y")){
 } else if(a5.toLocaleLowerCase() == ("no" || "n")) {
     alert('Correct');
     console.log('Fifth answer is correct');
+    correctAnswers++;
 } else {
     alert('Please use yes/no or y/n');
     console.log('Wrong value');
 }
 
-alert('Thanks '+userName+' for your answers, come back later for more quizes');
+//Update for the about me game, adding q6 + q7
+
+
+for(var q6_Attempts = 1; q6_Attempts <= 4; q6_Attempts++)
+{
+    var a6 = prompt("What rating would i give for game called 'Squad', rating should be between 0-10");
+    if(a6 == '8'){
+        alert('Correct');
+        console.log('Sixth answer is correct');
+        correctAnswers++;
+        break;
+    }else {
+        alert('Try again this is your '+q6_Attempts+'/4 attempt.');
+        if(q6_Attempts == '4'){
+            alert('You used all your opportunities, the correct answer is 8.');
+        }
+    }
+}
+
+for(var q7_Attempts = 1; q7_Attempts <= 6; q7_Attempts++)
+{
+    var a7 = prompt("Name one of the top four games i love!");
+    var i = 0; // counter to check all array topGames data.
+
+    for(i; i <= topGames.length; i++){
+    if(a7 == topGames[i]){
+        break;
+    }}
+
+    if(a7 == topGames[i]){
+        alert('Correct, you got one of the correct answers, these the top 4 games i love Arma, GTA, Squad and Fifa.');
+        console.log('Seventh answer is correct');
+        correctAnswers++;
+        break;
+    }else {
+        alert('Try again this is your '+q7_Attempts+'/6 attempt.');
+        if(q7_Attempts == '6'){
+            alert('You used all your opportunities, the correct answers are Arma, GTA, Squad and Fifa.');
+        }
+    }
+}
+
+
+alert('Thanks '+userName+' for your answers, you got '+correctAnswers+'/7, come back later for more quizes');
+
+
